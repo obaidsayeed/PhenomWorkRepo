92016db0d4fecb2afe9655dbc0f73f3c41fd9105
obaidsayeed <67325304+obaidsayeed@users.noreply.github.com>
2022-05-05 11:32:57 +0530

    Add files via upload

---
+++ StringConcepts/src/StringExample.java
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String args[]){
+        String s = "Hello World";
+        String s1 = new String("Hello World");
+        System.out.println(s + " " + s1);
+    }
+}
